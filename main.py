549a2ab00d7ac87c9ac81f602667c6478f53f307
Estevan Ruiz <econ.eruiz@gmail.com>
2017-06-09 12:43:33 -0500

    Finished assignment

--- main.py
+++ main.py
@@ -14,3 +14,52 @@ app.config['DEBUG'] = True
 def index():
     template = jinja_env.get_template('form.html')
     return template.render()
+
+@app.route("/", methods=['POST'])
+def validate_input():
+
+    username=request.form['username']
+    password=request.form['password']
+    verify=request.form['verify']
+    email=request.form['email']
+
+    username_error = ''
+    password_error = ''
+    verify_error = ''
+    email_error = ''
+
+    if (len(username) < 3) or (" " in username):
+        username_error = 'Not a valid username'
+        username = ''
+
+    if (len(password) < 3) or (" " in password):
+        password_error = 'Not a valid password'
+        password = ''
+
+    if not password == verify:
+        verify_error = 'Passwords do not match'
+        verify = ''
+
+    if len(email) > 0:
+        if not ("@" in email) or not ("." in email):
+            email_error = 'Not a valid email'
+            email = ''
+
+    if not username_error and not password_error and not verify_error and not email_error:
+        username = username
+        return redirect('/success?username={0}'.format(username))
+    else:
+        template = jinja_env.get_template('form.html')
+        return template.render(username_error=username_error, password_error=password_error, verify_error=verify_error, email_error=email_error, username=username, password=password, verify=verify, email=email)
+
+    
+
+
+@app.route('/success')
+def valid_username():
+    username = request.args.get('username')
+    return '<h1>Welcome, {0}.</h1>'.format(username)
+
+
+
+app.run()
